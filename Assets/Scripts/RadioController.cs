be5225b78fb0fcba185ebd65c7b48fa57acaae3d
FranBowden <francesca.bowden22@bathspa.ac.uk>
2024-10-23 18:48:51 +0100

    Created a Demo Scene
    Testing radio and gravity functionality

---
+++ Assets/Scripts/RadioController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioController : MonoBehaviour
+{
+    public AudioSource[] RadioTracks;
+    int trackNum = 0;
+    bool isRadioOn = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.tag == "Player") //if player collides with the radio
+        {
+            if (!isRadioOn) //if the radio is off
+            {
+                Debug.Log("Playing Radio");
+                RadioTracks[trackNum].Play(); //play the first track
+                isRadioOn = true;
+            }
+
+            else if (isRadioOn && trackNum < RadioTracks.Length - 1)
+            {
+                Debug.Log("Increasing Track");
+
+                RadioTracks[trackNum].Stop(); //stop the music
+                trackNum++; //increase the track by one
+                RadioTracks[trackNum].Play(); //play the new track 
+            }
+
+            else //if radio is on the last track
+            {
+                Debug.Log("Stopping Radio");
+
+                RadioTracks[trackNum].Stop(); //stop the music
+                trackNum = 0; //reset the track
+                isRadioOn = false;
+            }
+        }
+    }
+    
+}
