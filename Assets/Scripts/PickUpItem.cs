99e13a759c60f44fc713d5a9a3114f217bd4db78
FranBowden <francesca.bowden22@bathspa.ac.uk>
2024-11-20 18:38:56 +0000

    distortion of item picked up bug fixed

--- Assets/Scripts/PickUpItem.cs
+++ Assets/Scripts/PickUpItem.cs
@@ -15,6 +15,9 @@ public class PickUpItem : MonoBehaviour
     public PlayerControls interaction;
     private InputAction dropItemBtn;
 
+    Vector3 originalScale;
+
+
     private void Awake()
     {
         interaction = new PlayerControls();
@@ -49,14 +52,13 @@ public class PickUpItem : MonoBehaviour
         {
             if (!itemInHand) //picked up
             {
-
                 grabItem();
             } else
             {
                 if (currentItem.GetComponent<Rigidbody>()) //check if the current item has a rigidbody first to stop any errosr
                 {
                     //drop item
-                    dropItem(currentItem.GetComponent<Rigidbody>());
+                    dropItem(); 
                 }
             }
 
@@ -65,7 +67,7 @@ public class PickUpItem : MonoBehaviour
     }
 
    //this is being called with interaction script
-    public void grabItem()
+     void grabItem()
         {
       
             if (currentItem != null && currentItem.CompareTag("CanPickUp")) {
@@ -77,24 +79,50 @@ public class PickUpItem : MonoBehaviour
                 {
                     itemRigidbody.isKinematic = true;
                 }
+            originalScale = currentItem.transform.localScale;
+            currentItem.transform.SetParent(playerCamera.transform);
+            currentItem.transform.localPosition = new Vector3(0.5f, -1, 1);
+            currentItem.transform.localRotation = Quaternion.Euler(-90, 0, 0);
+            currentItem.transform.localScale = originalScale; // Restore original scale
 
-                //positions where the item is picked up in the camera view
-                currentItem.transform.SetParent(playerCamera.transform);
-                currentItem.transform.localPosition = new Vector3(0.5f, -1, 1);
-                currentItem.transform.localRotation = Quaternion.Euler(-90, 0, 0);
-            }
+            /*
+            //positions where the item is picked up in the camera view
+            currentItem.transform.SetParent(playerCamera.transform);
+            currentItem.transform.localPosition = new Vector3(0.5f, -1, 1);
+            currentItem.transform.localRotation = Quaternion.Euler(-90, 0, 0);
+            */
+        }
         
         
           
        }
 
        //drops the item
-    void dropItem(Rigidbody currentItem)
+    void dropItem()
     {
+        /*
             Debug.Log("Dropitem running");
             currentItem.isKinematic = false;
             currentItem.velocity = Vector3.zero;
             currentItem.transform.SetParent(null);
+        */
+        if (currentItem != null)
+        {
+            Rigidbody itemRigidbody = currentItem.GetComponent<Rigidbody>();
+
+            if (itemRigidbody != null)
+            {
+                itemRigidbody.isKinematic = false;
+                itemRigidbody.velocity = Vector3.zero;
+            }
+
+            // Unparent the item
+            currentItem.transform.SetParent(null);
+            currentItem.transform.localScale = originalScale;
+
+            currentItem = null;
             itemInHand = false;
+        }
     }
+
 }
