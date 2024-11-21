102d6b20b3f47726e8f78171fa204142693ded6e
FranBowden <francesca.bowden22@bathspa.ac.uk>
2024-11-21 12:43:59 +0000

    Radio displays song in text

--- Assets/Scripts/RadioController.cs
+++ Assets/Scripts/RadioController.cs
@@ -1,33 +1,33 @@
 
 using UnityEngine;
-
+using TMPro;
 public class RadioController : MonoBehaviour
 {
     public AudioSource[] RadioTracks;
     public int trackNum = 0;
     public bool isRadioOn = false;
-    public alienBehaviour AB;
+//    public alienBehaviour AB;
     public taskTerminal TT;
+    public TMPro.TextMeshPro textMeshPro;
     MainUIManager UIManager;
     private void Start()
     {
         UIManager = FindAnyObjectByType<MainUIManager>();
+        textMeshPro.text = ""; //turn off any text dispaly
     }
 
     public void TurnRadioOn() {
-        if (AB.isFollowing == true)
+       /* if (AB.isFollowing == true)
         {
             AB.radioChanged = true;
             print(AB.radioChanged);
-        }
-
-
-
+        }*/
 
             if (!isRadioOn) //if the radio is off
             {
                 Debug.Log("Playing Radio");
                 RadioTracks[trackNum].Play(); //play the first track
+                textMeshPro.text = "Playing: Song " + (trackNum + 1);
                 isRadioOn = true;
 
             }
@@ -37,10 +37,12 @@ public class RadioController : MonoBehaviour
                 Debug.Log("Increasing Track");
 
                 RadioTracks[trackNum].Stop(); //stop the music
+               // textMeshPro.text = "";
                 trackNum++; //increase the track by one
                 RadioTracks[trackNum].Play(); //play the new track 
+                textMeshPro.text = "Playing: Song " + (trackNum + 1);
 
-            }
+        }
 
             else //if radio is on the last track
             {
@@ -49,7 +51,8 @@ public class RadioController : MonoBehaviour
                 RadioTracks[trackNum].Stop(); //stop the music
                 trackNum = 0; //reset the track
                 isRadioOn = false;
-            }
+                textMeshPro.text = "";
+        }
 
             if (TT.radioTaskActive)
         {
