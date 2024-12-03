99642d461f380e00a78a78dd16bfac7b89fd5be6
Francesca Bowden <francesca.bowden22@bathspa.ac.uk>
2024-12-03 10:01:39 +0000

    Cat AI Nav

--- Assets/Scripts/CatMovement.cs
+++ Assets/Scripts/CatMovement.cs
@@ -6,10 +6,30 @@ using UnityEngine.AI;
 
 public class CatMovement : MonoBehaviour
 {
-    public NavMeshAgent cat;
-    public Transform player;
-    void Update()
+    private NavMeshAgent agent;
+    private int randomNum;
+    public Transform[] destinations;
+
+    //cat movement
+
+    private void Start()
     {
-        cat.SetDestination(player.position);
+        agent = GetComponent<NavMeshAgent>();
+        StartCoroutine(GenerateRandomNum()); //generate a random number very 40 seconds
     }
+
+
+    private void Update()
+    {
+        agent.destination = destinations[randomNum].position;
+    }
+
+    IEnumerator GenerateRandomNum()
+    {
+        randomNum = Random.Range(0, destinations.Length);
+
+        Debug.Log("Generated arndom number");
+        yield return new WaitForSeconds(40f);
+    }
+
 }
