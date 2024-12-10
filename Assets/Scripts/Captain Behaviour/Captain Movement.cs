5d226b5b98db5b0cf2277fb7d03136c6f32d6821
Francesca Bowden <francesca.bowden22@bathspa.ac.uk>
2024-12-10 15:07:23 +0000

    imported steam audio package

--- Assets/Scripts/Captain Behaviour/Captain Movement.cs
+++ Assets/Scripts/Captain Behaviour/Captain Movement.cs
@@ -6,29 +6,4 @@ using static UnityEngine.EventSystems.EventTrigger;
 public class CaptainMovement : MonoBehaviour
 {
 
-
-    private Vector3 centre;
-    public float radius = 10f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        centre.x = this.gameObject.transform.position.x;
-        centre.y = this.gameObject.transform.position.y;
-        centre.z = this.gameObject.transform.position.z;
-
-        Collider[] objectsInRange = Physics.OverlapSphere(centre, radius);
-        for (int i = 0; i < objectsInRange.Length; i++)
-        {
-            if (objectsInRange[i].tag == "Player")
-            {
-                Debug.Log("Player is near by");
-            }
-        }
-    }
 }
