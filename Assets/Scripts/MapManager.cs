5821ee30a4d6e2444ac598dd8573ae0fd00fc536
FranBowden <francesca.bowden22@bathspa.ac.uk>
2025-01-10 16:22:26 +0000

    Updated map

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -1,81 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MapManager : MonoBehaviour
 {
-    public Transform player;          // Reference to the player in the 3D world
-    public RectTransform playerPanel; // The 2D panel representing the player
-    public Transform shipInterior;    // The ship's interior GameObject
 
-    private Bounds shipBounds;        // Bounds of the ship
-
-
-
-    private RawImage map;
+    [SerializeField] private GameObject Timer;
+    [SerializeField] private GameObject interactionText;
+    [SerializeField] private GameObject DialogueBoxAlien;
+    [SerializeField] private GameObject DialogueBoxCaptain;
 
     private void Start()
     {
-        map = GetComponent<RawImage>();
-
-        map.gameObject.SetActive(false);
-
-
-        shipBounds = new Bounds(shipInterior.position, Vector3.zero);
-
-        foreach (Renderer renderer in shipInterior.GetComponentsInChildren<Renderer>())
-        {
-            shipBounds.Encapsulate(renderer.bounds);
-        }
+        gameObject.SetActive(false);
     }
-
-
-    private void Update()
-    {
-        UpdatePlayerPanelPosition();
-    }
-
-    //the math is off but the idea is the 2d panel would be the player
-    private void UpdatePlayerPanelPosition()
-    {
-        // Get the player's position
-        Vector3 playerPosition = player.position;
-
-        // Normalize the player's position within the ship bounds
-        float normalizedX = Mathf.Clamp01((playerPosition.x - shipBounds.min.x) / shipBounds.size.x);
-        float normalizedY = Mathf.Clamp01((playerPosition.z - shipBounds.min.z) / shipBounds.size.z);
-
-        // Convert normalized coordinates to map coordinates
-        float mapX = normalizedX * map.rectTransform.rect.width - (map.rectTransform.rect.width / 2f);
-        float mapY = normalizedY * map.rectTransform.rect.height - (map.rectTransform.rect.height / 2f);
-
-        // Update the position of the player panel without affecting rotation
-        Vector2 mapPosition = new Vector2(mapX, mapY);
-
-        // Directly set anchoredPosition to ignore rotation (will only update position, not rotation)
-        playerPanel.anchoredPosition = mapPosition;
-
-        // Optional: if you want to make sure rotation is reset, you can explicitly set it to zero
-        playerPanel.rotation = Quaternion.identity; // This will reset any rotation applied to the player panel
-    }
-
     public void ToggleMap()
     {
-
-        if (map == null)
+        if (gameObject.activeSelf)
         {
-            Debug.LogError("Map is null! Ensure it is assigned in the Inspector or code.");
-            return;
-        }
+            gameObject.SetActive(false);
+            Timer.SetActive(true);
+            DialogueBoxAlien.SetActive(true);
+            DialogueBoxCaptain.SetActive(true);
+            interactionText.SetActive(true);
 
-        if (map.gameObject.activeSelf)
-        {
-            map.gameObject.SetActive(false); // If currently active, deactivate it
         }
         else
         {
-            map.gameObject.SetActive(true); // If currently inactive, activate it
+            gameObject.SetActive(true);
+            Timer.SetActive(false);
+            DialogueBoxAlien.SetActive(false);
+            DialogueBoxCaptain.SetActive(false);
+            interactionText.SetActive(false);
+
+
         }
     }
 
