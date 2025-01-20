c425d9ce714beb44579a1837dee03df703335a45
McEw3965 <EWAN.MCCARNEY22@bathspa.ac.uk>
2025-01-20 14:19:26 +0000

    Door now opens when switch pressed

--- Assets/incineratorSwitch.cs
+++ Assets/incineratorSwitch.cs
@@ -2,21 +2,21 @@ using UnityEngine;
 
 public class incineratorSwitch : MonoBehaviour
 {
+    [SerializeField] private GameObject Doors;
+    private Animator doorAnimator;
     [SerializeField] private GameObject leftDoor;
     [SerializeField] private GameObject rightDoor;
     private bool doorsOpen;
-    private Vector3 leftInitialPos;
-    private Vector3 rightInitialPos;
+    public Vector3 leftInitialPos;
+    public Vector3 rightInitialPos;
+
 
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Vector3 leftInitialPos = leftDoor.GetComponent<Transform>().localPosition;
-        Vector3 rightInitialPos = rightDoor.GetComponent<Transform>().localPosition;
-        Debug.Log("left initial Pos" + leftInitialPos);
-        Debug.Log("Right Initial Pos" + rightInitialPos);
+        doorAnimator = Doors.GetComponent<Animator>();
 
     }
 
@@ -28,23 +28,14 @@ public class incineratorSwitch : MonoBehaviour
 
     public void openDoors()
     {
-        Debug.Log("left initial Pos" + leftInitialPos);
-
         if (!doorsOpen)
         {
-
-            Debug.Log("Doors Open");
-            leftDoor.GetComponent<Transform>().localPosition = new Vector3(leftInitialPos.x + 4f, leftInitialPos.y, leftInitialPos.z);
-            rightDoor.GetComponent<Transform>().localPosition = new Vector3(rightInitialPos.x - 4f, rightInitialPos.y, rightInitialPos.z);
+            doorAnimator.SetBool("IsDoorOpen?", true);
             doorsOpen = true;
-
         } else if (doorsOpen)
         {
-            Debug.Log("Doors Closed");
-            leftDoor.GetComponent<Transform>().localPosition = new Vector3(leftInitialPos.x, leftInitialPos.y, leftInitialPos.z);
-            rightDoor.GetComponent<Transform>().localPosition = new Vector3(rightInitialPos.x, rightInitialPos.y, rightInitialPos.z);
+            doorAnimator.SetBool("IsDoorOpen?", false);
             doorsOpen = false;
-
         }
 
     }
