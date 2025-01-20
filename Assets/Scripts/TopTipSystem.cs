673de6cdd45f82dc4347c66a87e284fdc240a512
FranBowden <francesca.bowden22@bathspa.ac.uk>
2025-01-20 17:02:30 +0000

    Top Tips have been added to game

---
+++ Assets/Scripts/TopTipSystem.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+
+public class TopTipSystem : MonoBehaviour
+{
+    [SerializeField] private GameObject torchTip;
+    [SerializeField] private GameObject SprintTip;
+
+    bool torchTipDisplayed = false;
+    bool sprintTipDisplayed = false;
+
+    private void Start()
+    {
+
+        StartCoroutine(DisplayTorchTip());
+        StartCoroutine(DisplaySprintTip());
+    }
+
+    private IEnumerator DisplayTorchTip()
+    {
+        yield return new WaitForSeconds(5f);
+
+        if(!torchTipDisplayed)
+        {
+            torchTipDisplayed = true;
+            torchTip.SetActive(true);
+            yield return new WaitForSeconds(5f);
+            torchTip.SetActive(false);
+        }
+
+    }
+
+
+
+
+
+    private IEnumerator DisplaySprintTip()
+    {
+        yield return new WaitForSeconds(10f);
+
+        if (!torchTipDisplayed)
+        {
+            sprintTipDisplayed = true;
+            SprintTip.SetActive(true);
+            yield return new WaitForSeconds(5f);
+            SprintTip.SetActive(false);
+        }
+
+    }
+}
